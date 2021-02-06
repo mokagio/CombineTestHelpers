943b8b1351941c57096711635b7f0306dfd52858
Gio Lodi <giovanni.lodi42@gmail.com>
2021-02-07 06:24:17 +1100

    Dry integration test Project.swift

--- IntegrationTests/Project.swift
+++ IntegrationTests/Project.swift
@@ -1,6 +1,30 @@
 import ProjectDescription
 
-let dependencies: [TargetDependency] = [.package(product: "CombineTestHelpers")]
+func testTarget(for platform: Platform) -> Target {
+    let bundleIdRoot: String
+    let deploymentTarget: DeploymentTarget
+    switch platform {
+    case .iOS:
+        bundleIdRoot = "ios"
+        deploymentTarget = .iOS(targetVersion: "13.0", devices: [.iphone, .ipad])
+    case .macOS:
+        bundleIdRoot = "macos"
+        deploymentTarget = .macOS(targetVersion: "10.15")
+    case _:
+        fatalError("Not yet supported")
+    }
+
+    return Target(
+        name: "\(bundleIdRoot)Tests",
+        platform: platform,
+        product: .unitTests,
+        bundleId: "\(bundleIdRoot).mokagio.CombineTestHelpers",
+        deploymentTarget: deploymentTarget,
+        infoPlist: "Info.plist",
+        sources: ["Tests/**"],
+        dependencies: [.package(product: "CombineTestHelpers")]
+    )
+}
 
 let project = Project(
     name: "IntegrationTests",
@@ -9,25 +33,7 @@ let project = Project(
     // parent folder of this project specification.
     packages: [.package(path: "../../CombineTestHelpers")],
     targets: [
-        Target(
-            name: "iosTests",
-            platform: .iOS,
-            product: .unitTests,
-            bundleId: "ios.mokagio.CombineTestHelpers",
-            deploymentTarget: .iOS(targetVersion: "13.0", devices: [.iphone, .ipad]),
-            infoPlist: "Info.plist",
-            sources: ["Tests/**"],
-            dependencies: dependencies
-        ),
-        Target(
-            name: "macosTests",
-            platform: .macOS,
-            product: .unitTests,
-            bundleId: "macos.mokagio.CombineTestHelpers",
-            deploymentTarget: .macOS(targetVersion: "10.15"),
-            infoPlist: "Info.plist",
-            sources: ["Tests/**"],
-            dependencies: dependencies
-        ),
+        testTarget(for: .iOS),
+        testTarget(for: .macOS),
     ]
 )
