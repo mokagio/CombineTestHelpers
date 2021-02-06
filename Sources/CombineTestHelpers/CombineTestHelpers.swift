f3db4d7f775f2169cfac23dacb3679e7cb42c6a2
Gio Lodi <giovanni.lodi42@gmail.com>
2021-02-07 05:43:57 +1100

    Make XCTestCase extension public

--- Sources/CombineTestHelpers/CombineTestHelpers.swift
+++ Sources/CombineTestHelpers/CombineTestHelpers.swift
@@ -1,7 +1,7 @@
 import Combine
 import XCTest
 
-extension XCTestCase {
+public extension XCTestCase {
 
     func doesThisCompile() -> AnyPublisher<Int, Error> {
         Result<Int, Error>.success(42).publisher.eraseToAnyPublisher()
